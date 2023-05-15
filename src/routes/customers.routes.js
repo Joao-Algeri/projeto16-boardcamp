d3d3b2929a679849ed1bb5ab7e19fbf62408542f
JoaoAlgeri <joaoalgeri@gmail.com>
2023-05-15 14:30:27 -0300

    Feat:update customers

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { insertCustomer, getCustomers, getCustomersId} from '../controllers/customers.controller.js'
+import { insertCustomer, getCustomers, getCustomersId, updateCustomer } from '../controllers/customers.controller.js'
 import { validateCustomer } from '../middlewares/customers.middleware.js'
 
 const customerRouter = Router()
@@ -7,6 +7,6 @@ const customerRouter = Router()
 customerRouter.get('/customers', getCustomers)
 customerRouter.get('/customers/:id', getCustomersId)
 customerRouter.post('/customers', validateCustomer, insertCustomer)
-
+customerRouter.put('/customers/:id', validateCustomer, updateCustomer)
 
 export default customerRouter
